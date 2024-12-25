0e71374c18d3d38cb8d9c56bbd8eb8612ae1ffa0
DhananjayaDilz <dhananjaya.20221596@iit.ac.lk>
2024-12-25 20:05:23 +0530

    set mobile responsive at service section page

--- src/components/ServiceSectionCard.jsx
+++ src/components/ServiceSectionCard.jsx
@@ -1,19 +1,19 @@
-const serviceSectionCard = ({Icon:Icon, title, desctiption}) => {
+const ServiceSectionCard = ({ Icon, title, description }) => {
     return (
         <div
-            className="flex p-[1rem] cursor-default justify-center items-center gap-[1.25rem] rounded-[0.625rem] border border-[#042630] bg-[#042630]">
-            <Icon className="text-[#f3f3f5] text-[3rem]"/>
+            className="flex p-4 sm:p-[1rem] cursor-default justify-center items-center gap-4 sm:gap-[1.25rem] rounded-md sm:rounded-[0.625rem] border border-[#042630] bg-[#042630] flex-wrap">
+            <Icon className="text-[#f3f3f5] text-2xl sm:text-[3rem]" />
             <div>
                 <h6
-                    className="flex w-[17.58rem] h-[1.5rem] flex-col justify-center flex-shrink-0 text-[#F3F3F5] font-dm-sans text-[1.5rem] font-medium leading-[1.5rem]">
+                    className="w-full sm:w-[17.58rem] text-[#F3F3F5] font-dm-sans text-lg sm:text-[1.5rem] font-medium leading-snug">
                     {title}
                 </h6>
-                <p className="flex w-[17.58rem] h-[4.1875rem] flex-col justify-center flex-shrink-0 text-[#F3F3F5] font-dm-sans text-[1rem] font-normal leading-[1rem]">
-                    {desctiption}
+                <p className="w-full sm:w-[17.58rem] text-[#F3F3F5] font-dm-sans text-sm sm:text-[1rem] font-normal leading-relaxed">
+                    {description}
                 </p>
             </div>
         </div>
     );
-}
+};
 
-export default serviceSectionCard;
+export default ServiceSectionCard;
