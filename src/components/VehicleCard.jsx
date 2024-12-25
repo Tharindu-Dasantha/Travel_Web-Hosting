2f1a2a06e0abf932b9da57ad86bb6b166020efb6
DhananjayaDilz <dhananjaya.20221596@iit.ac.lk>
2024-12-25 20:04:39 +0530

    set mobile responsive at vehicle section page

--- src/components/VehicleCard.jsx
+++ src/components/VehicleCard.jsx
@@ -1,29 +1,31 @@
-export const LargeVehicleCard = ({icon, title, image}) => {
+export const LargeVehicleCard = ({ icon, title, image }) => {
     return (
         <div
-            className={`w-[35rem] h-[18rem] bg-no-repeat bg-cover rounded-[1rem] relative transition-transform duration-300 hover:scale-105 cursor-default`} style={{backgroundImage: `url(${image})`}}>
-            <div
-                className="absolute bottom-0 left-0 m-2 flex p-[0.3125rem] px-[1.85156rem] pl-[1.875rem] justify-center items-center gap-[0.625rem] flex-shrink-0 rounded-[2.75rem] bg-[#FFF]">
-                <img src={icon} alt="icon"/>
-                <p className="flex flex-col justify-center flex-shrink-0 text-[#050B20] font-dm-sans text-[0.9375rem] font-medium leading-[1.125rem]">
+            className={`relative w-full sm:w-[35rem] h-[14rem] sm:h-[18rem] bg-no-repeat bg-cover rounded-[1rem] transition-transform duration-300 hover:scale-105 cursor-pointer`}
+            style={{ backgroundImage: `url(${image})` }}
+        >
+            <div className="absolute bottom-0 left-0 m-2 flex p-2 sm:px-4 justify-center items-center gap-2 rounded-full bg-white">
+                <img src={icon} alt="icon" className="w-6 h-6 sm:w-8 sm:h-8" />
+                <p className="text-sm sm:text-base text-[#050B20] font-dm-sans font-medium">
                     {title}
                 </p>
             </div>
         </div>
     );
-}
+};
 
-export const SmallVehicleCard = ({icon, title, image}) => {
+export const SmallVehicleCard = ({ icon, title, image }) => {
     return (
         <div
-            className={`relative w-[22.56rem] h-[18rem] bg-no-repeat bg-cover rounded-[1rem] transition-transform duration-300 hover:scale-105 cursor-default`} style={{backgroundImage: `url(${image})`}}>
-            <div
-                className="absolute bottom-0 left-0 m-2 flex p-[0.3125rem] px-[1.85156rem] pl-[1.875rem] justify-center items-center gap-[0.625rem] flex-shrink-0 rounded-[2.75rem] bg-[#FFF]">
-                <img src={icon} alt="icon"/>
-                <p className="flex flex-col justify-center flex-shrink-0 text-[#050B20] font-dm-sans text-[0.9375rem] font-medium leading-[1.125rem]">
+            className={`relative w-full sm:w-[22.56rem] h-[14rem] sm:h-[18rem] bg-no-repeat bg-cover rounded-[1rem] transition-transform duration-300 hover:scale-105 cursor-pointer`}
+            style={{ backgroundImage: `url(${image})` }}
+        >
+            <div className="absolute bottom-0 left-0 m-2 flex p-2 sm:px-4 justify-center items-center gap-2 rounded-full bg-white">
+                <img src={icon} alt="icon" className="w-6 h-6 sm:w-8 sm:h-8" />
+                <p className="text-sm sm:text-base text-[#050B20] font-dm-sans font-medium">
                     {title}
                 </p>
             </div>
         </div>
     );
-}
+};
--- src/components/VehicleSection.jsx
+++ src/components/VehicleSection.jsx
@@ -1,4 +1,4 @@
-import {LargeVehicleCard, SmallVehicleCard} from "./VehicleCard.jsx";
+import { LargeVehicleCard, SmallVehicleCard } from "./VehicleCard.jsx";
 
 // importing images
 import suv from '/suv.svg';
@@ -13,26 +13,23 @@ const coupeImage = '/coupe.jpeg';
 const hatchbackImage = '/hatchback.jpeg';
 const hybridImage = '/hybrid.jpeg';
 
-
-
 const VehicleSection = () => {
     return (
-        <div
-            className="flex p-[6.25rem] px-[0.0625rem] flex-col justify-center items-center gap-[3.125rem] bg-[#FFF]">
-            <div className="flex p-[1.5625rem] px-[9.375rem] items-center gap-[63.3125rem] bg-[#FFF]">
-                <h3 className="text-[#000] font-odor-mean-chey text-[4rem] font-normal leading-normal">
+        <div className="flex flex-col p-8 sm:p-16 justify-center items-center gap-12 bg-white">
+            <div className="flex justify-center items-center">
+                <h3 className="text-black text-2xl sm:text-4xl font-odor-mean-chey font-normal">
                     Available Vehicles
                 </h3>
             </div>
-            <div className="flex w-[80%] items-end justify-center content-end gap-[1.875rem] flex-wrap">
-                <LargeVehicleCard icon={suv} title={"SUV"} image={suvImage} />
-                <LargeVehicleCard icon={sedan} title={"Sedan"} image={sedanImage} />
-                <SmallVehicleCard icon={coupe} title={"Hatchback"} image={hatchbackImage} />
-                <SmallVehicleCard icon={hatchback} title={"Hybrid"} image={hybridImage} />
-                <SmallVehicleCard icon={hybrid} title={"Coupe"} image={coupeImage} />
+            <div className="flex w-full sm:w-[80%] justify-center flex-wrap gap-6">
+                <LargeVehicleCard icon={suv} title="SUV" image={suvImage} />
+                <LargeVehicleCard icon={sedan} title="Sedan" image={sedanImage} />
+                <SmallVehicleCard icon={coupe} title="Hatchback" image={hatchbackImage} />
+                <SmallVehicleCard icon={hatchback} title="Hybrid" image={hybridImage} />
+                <SmallVehicleCard icon={hybrid} title="Coupe" image={coupeImage} />
             </div>
         </div>
     );
-}
+};
 
 export default VehicleSection;
